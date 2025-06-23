bc2abc5c77997189c5d1095fe504ba79e1fafec2
Luca Cocchia <lucacocchia1234@gmail.com>
2025-06-22 19:20:50 -0500

    init

---
+++ src/main/java/net/blazeinception/celestforge/CelestforgeClient.java
@@ -0,0 +1,10 @@
+package net.blazeinception.celestforge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CelestforgeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/blazeinception/celestforge/CelestforgeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.blazeinception.celestforge;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CelestforgeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
